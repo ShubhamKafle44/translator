b8a0074f48f61dcbabf2201bbacf6efedd532d49
Shubham Kafle <shubhamkafle44@gmail.com>
2024-12-15 11:58:21 -0600

    Package Update and tailwind css

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI, BackgroundTasks, HTTPException, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
-
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory = "static"),name ="static")
 
 templates = Jinja2Templates(directory = "templates")
 
---
+++ app/tailwindcss/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../tempelates/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
